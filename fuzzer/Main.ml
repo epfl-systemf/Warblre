5d103a934837791585e370f9b187fdbe33c6bd32
Aurèle <aurele.barriere@epfl.ch>
2023-12-14 10:06:25 +0100

    fixed JS pretty printing of regexes

--- fuzzer/Main.ml
+++ fuzzer/Main.ml
@@ -3,7 +3,7 @@ open Extracted
 open Patterns
 open Notations
 open Helpers
-open Random
+(* open Random *)
 
 (** * JS Regex pretty-printing *)
 (* adding a non-capturing group to a string *)
@@ -27,14 +27,14 @@ let quantifier_to_string (q:coq_Quantifier) : string =
 let rec regex_to_string (r:coq_Regex) : string =
   match r with
   | Empty -> ""
-  | Char (_, _) -> failwith "todo"
+  | Char (c) -> String.make 1 c
   | Disjunction (r1, r2) -> noncap(regex_to_string r1) ^ "|" ^ noncap(regex_to_string r2)
   | Quantified (r1, q) -> noncap(regex_to_string r1) ^ quantifier_to_string q
   | Seq (r1, r2) -> noncap(regex_to_string r1) ^ noncap(regex_to_string r2)
-  | Group (_,r1) -> "("^ regex_to_string r1 ^")" (* ignoring the annotation *)
+  | Group (_,r1) -> "("^ regex_to_string r1 ^")" (* ignoring the name for now *)
   | Lookahead (r1) -> "(?="^ regex_to_string r1 ^")"
   | NegativeLookahead (r1) -> "(?!"^ regex_to_string r1 ^")"
-  | Lookback (r1) -> "(?<="^ regex_to_string r1 ^ ")"
+  | Lookbehind (r1) -> "(?<="^ regex_to_string r1 ^ ")"
   | NegativeLookbehind (r1) -> "(?<!"^ regex_to_string r1 ^")"
   | BackReference (gid) -> "\\"^ string_of_int gid
 
@@ -42,19 +42,23 @@ let rec regex_to_string (r:coq_Regex) : string =
 
 (* we restrict ourselves to a small alphabet *)
 (* with a dash (non-ascii) to test word boundaries *)
-let alphabet = ['a'; 'b'; '-']
+(* let alphabet = ['a'; 'b'; '-'] *)
 
-let max_depth = 50
+(* let max_depth = 50 *)
 
-let max_string = 100
+(* let max_string = 100 *)
 
 
 
 
 let str = "aaaaabaac"
-let regex = Group (0, !* (
+let regex = Group (None, !* (
     Disjunction (
-      Group (1, char 'a'),
-      Group (2, char 'b'))))
+      Group (None, char 'a'),
+      Group (None, char 'b'))))
 
-let () = test_regex regex str 0
+let () =
+  let js_regex = regex_to_string regex in
+  Printf.printf "regex: %s\n" js_regex; 
+  test_regex regex str 0;
+  Printf.printf "done\n"

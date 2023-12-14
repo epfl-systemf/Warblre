f28921517cb584bb62fee0392b0def0c6e76ef09
Aurèle <aurele.barriere@epfl.ch>
2023-12-14 11:46:01 +0100

    random quantifiers

--- fuzzer/Main.ml
+++ fuzzer/Main.ml
@@ -1,9 +1,8 @@
 open Warblre
-open Extracted
-open Patterns
+open Extracted.Patterns
 open Notations
 open Helpers
-(* open Random *)
+
 
 (** * JS Regex pretty-printing *)
 (* adding a non-capturing group to a string *)
@@ -39,10 +38,45 @@ let rec regex_to_string (r:coq_Regex) : string =
   | BackReference (gid) -> "\\"^ string_of_int gid
 
 
-
+(** * Generating random regexes *)
 (* we restrict ourselves to a small alphabet *)
 (* with a dash (non-ascii) to test word boundaries *)
-(* let alphabet = ['a'; 'b'; '-'] *)
+let alphabet : char list = ['a'; 'b'; '-']
+
+let random_char () : char =
+  let idx = Random.int (List.length alphabet) in
+  List.nth alphabet idx
+
+(* maximum bound for counted repetition *)
+let max_counted = 10
+
+let random_qp () : coq_QuantifierPrefix =
+  match (Random.int 6) with
+  | 0 -> Star
+  | 1 -> Plus
+  | 2 -> Question
+  | 3 -> let rep = Random.int max_counted in
+         RepExact(rep)
+  | 4 -> let rep = Random.int max_counted in
+         RepPartialRange(rep)
+  | 5 -> let minrep = Random.int max_counted in
+         let maxrep = minrep + Random.int max_counted in
+         RepRange(minrep, maxrep)
+  | _ -> failwith "random range error"
+
+let random_quant () : coq_Quantifier =
+  let qp = random_qp () in
+  if (Random.bool ()) then Greedy qp else Lazy qp
+
+
+(* We generate regexes in two steps *)
+(* First we generate a random AST, where backreferences are all set to 0 (an invalid backref index) *)
+(* Then we compute how many groups have been generated *)
+(* Next we replace the backreference indices by indices between 1 and the maximum group index *)
+(* If no groups are available, we replace backreferences with empty *)
+
+
+
 
 (* let max_depth = 50 *)
 
@@ -58,6 +92,8 @@ let regex = Group (None, !* (
       Group (None, char 'b'))))
 
 let () =
+  ignore (random_char());
+  ignore (random_quant());
   let js_regex = regex_to_string regex in
   Printf.printf "regex: %s\n" js_regex; 
   test_regex regex str 0;

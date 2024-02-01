c7d03656785879b64a7c8f9a26e1c2197d071086
Aurèle <aurele.barriere@epfl.ch>
2024-02-01 12:10:22 +0100

    fixed tests when renaming boundary assertions

--- test/BoundaryAssertions.ml
+++ test/BoundaryAssertions.ml
@@ -4,21 +4,21 @@ open Warblre.Notations
 
 let%expect_test "match_start_pos" =
   test_regex
-    (AssertInputStart -- char 'a')
+    (InputStart -- char 'a')
     "aa"
     0 ();
   [%expect {| Matched 1 characters ([0-1]) in 'aa' (length=2) |}]
 
 let%expect_test "match_start_neg_0" =
   test_regex
-    (AssertInputStart -- char 'a')
+    (InputStart -- char 'a')
     "aa"
     1 ();
   [%expect {| No match on 'aa' |}]
 
 let%expect_test "match_start_neg_1" =
   test_regex
-    ((char 'a') -- AssertInputStart -- (char 'a'))
+    ((char 'a') -- InputStart -- (char 'a'))
     "aa"
     0 ();
   [%expect {| No match on 'aa' |}]
@@ -27,21 +27,21 @@ let%expect_test "match_start_neg_1" =
 
 let%expect_test "match_end_pos" =
   test_regex
-    ((char 'a') -- AssertInputEnd)
+    ((char 'a') -- InputEnd)
     "aa"
     1 ();
   [%expect {| Matched 1 characters ([1-2]) in 'aa' (length=2) |}]
 
 let%expect_test "match_start_neg_0" =
   test_regex
-  ((char 'a') -- AssertInputEnd)
+  ((char 'a') -- InputEnd)
     "aa"
     0 ();
   [%expect {| No match on 'aa' |}]
 
 let%expect_test "match_start_neg_1" =
   test_regex
-    ((char 'a') -- AssertInputEnd -- (char 'a'))
+    ((char 'a') -- InputEnd -- (char 'a'))
     "aa"
     0 ();
   [%expect {| No match on 'aa' |}]
@@ -50,14 +50,14 @@ let%expect_test "match_start_neg_1" =
 
   let%expect_test "word_boundary_pos" =
   test_regex
-    ((char 'a') -- AssertWordBoundary -- (char ' '))
+    ((char 'a') -- WordBoundary -- (char ' '))
     "a a"
     0 ();
   [%expect {| Matched 2 characters ([0-2]) in 'a a' (length=3) |}]
 
 let%expect_test "word_boundary_neg" =
   test_regex
-    ((char 'a') -- AssertWordBoundary -- (char 'a'))
+    ((char 'a') -- WordBoundary -- (char 'a'))
     "aaa"
     0 ();
   [%expect {| No match on 'aaa' |}]
@@ -66,14 +66,14 @@ let%expect_test "word_boundary_neg" =
 
 let%expect_test "not_word_boundary_pos" =
   test_regex
-    ((char 'a') -- AssertNotWordBoundary -- (char 'a'))
+    ((char 'a') -- NotWordBoundary -- (char 'a'))
     "aaa"
     0 ();
   [%expect {| Matched 2 characters ([0-2]) in 'aaa' (length=3) |}]
 
 let%expect_test "not_word_boundary_neg" =
   test_regex
-    ((char 'a') -- AssertNotWordBoundary -- (char ' '))
+    ((char 'a') -- NotWordBoundary -- (char ' '))
     "a a"
     0 ();
   [%expect {| No match on 'a a' |}]

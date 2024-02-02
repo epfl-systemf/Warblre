dc45244226a1260bf76699d9c1853ccc8176352a
Aurèle <aurele.barriere@epfl.ch>
2024-02-02 18:52:01 +0100

    updated the proof plan, describing strictly nullable matchers and proving it with induction

--- MECMAR/props/StrictlyNullable.v
+++ MECMAR/props/StrictlyNullable.v
@@ -7,6 +7,7 @@ Import Coercions.
 Import Semantics.
 Import MatchState.
 Import Match.
+Import Correctness.MatchState.
 
 Local Open Scope result_flow.
 
@@ -29,8 +30,58 @@ Fixpoint strictly_nullable (r:Regex) : bool :=
 (* If it is, then so is the backreference *)
 (* For the Quantifier, we could see as striclty nullable repetitions like {0} and {0,0} regardless of the inner regex *)
 
+(** * Strictly Nullable Matchers  *)
+
+Definition strictly_nullable_matcher (m:Matcher)(rer:RegExp) : Prop :=
+  (* for any valid state x and continuation c and string str *)
+  forall x c (VALID: Valid (input x) rer x),
+    (* Then either the match fails *)
+    (m x c = failure) \/
+      (* or called its continuation c on some state that has he same index as x *)
+      (exists y, Valid (input x) rer y /\ endIndex x = endIndex y /\ c y = m x c).
+
+
+(** * Analysis Correctness  *)
+
+Theorem strictly_nullable_analysis_correct:
+  forall (r:Regex) (ctx:RegexContext) (rer:RegExp) (dir:direction) (m:Matcher)
+    (STRICTLY_NULLABLE: strictly_nullable r = true)
+    (COMPILE: compileSubPattern r ctx rer dir = Success m),
+    strictly_nullable_matcher m rer.
+Proof.
+  intros r. induction r; intros ctx rer dir m STRICTLY_NULLABLE COMPILE;
+    try solve[inversion STRICTLY_NULLABLE]; unfold strictly_nullable_matcher; intros x c VALID.
+  - simpl in COMPILE. inversion COMPILE as [H]. clear H COMPILE.
+    destruct (c x) eqn:CONT; auto.
+    + destruct s; auto. right.
+      exists x. split; auto.
+    + right. exists x. split; auto.
+  - simpl in COMPILE.
+    inversion STRICTLY_NULLABLE as [SN12]. rewrite andb_true_iff in SN12. destruct SN12 as [SN1 SN2].
+    clear STRICTLY_NULLABLE.
+    destruct (compileSubPattern r1 (Disjunction_left r2 :: ctx) rer dir) as [m1|] eqn:SNM1; try solve[inversion COMPILE].
+    apply IHr1 in SNM1; auto. clear IHr1 SN1.
+    destruct (compileSubPattern r2 (Disjunction_right r1 :: ctx) rer dir) as [m2|] eqn:SNM2; try solve[inversion COMPILE].
+    apply IHr2 in SNM2; auto. clear IHr2 SN2.
+    inversion COMPILE as [M]. clear COMPILE M.
+    (* first, excecute m1 *)
+    unfold strictly_nullable_matcher in SNM1.
+    specialize (SNM1 x c VALID). destruct SNM1 as [TRYRIGHT | [y [VALIDy [SAMEIDX EQUAL]]]].
+    +                           (* Matcher 1 failed, try matcher 2 *)
+      rewrite TRYRIGHT. simpl. apply SNM2; auto.
+    +                           (* Matcher 1 succeeded *)
+      rewrite <- EQUAL.
+      destruct (c y) as [res|] eqn:CONT.
+      * destruct res as [res|]; auto. right.
+        exists y. split; auto.
+      * right. exists y. split; auto.
+
+        (* This theorem seems provable by induction this time *)
+Admitted.
+
 
 (** * Intermediate lemmas  *)
+
 Lemma nil_range:
   forall x, List.Range.Nat.Bounds.range x x = nil.
 Proof.
@@ -46,14 +97,54 @@ Proof.
 Qed.
 
 
+(** * Repeating a strictly nullable matcher  *)
+Lemma strictly_nullable_repeatmatcher':
+  forall (r:Regex) (root:Regex) (ctx:RegexContext) (rer:RegExp) (dir:direction) (m:Matcher)
+    (STRICTLY_NULLABLE: strictly_nullable r = true)
+    (COMPILE: compileSubPattern r ctx rer dir = Success m)
+    (RER_ADEQUACY: countLeftCapturingParensWithin root nil = RegExp.capturingGroupsCount rer)
+    (ROOT: Root root r ctx)
+    (EARLY_ERRORS: EarlyErrors.Pass.Regex root nil),
+  forall (x:MatchState) (c:MatcherContinuation),
+    repeatMatcher' m O%nat NoI.Inf true x c (countLeftCapturingParensBefore r ctx) (countLeftCapturingParensWithin r ctx) (repeatMatcherFuel O%nat x) = c x.
+Proof.
+  intros r root ctx rer dir m STRICTLY_NULLABLE COMPILE RER_ADEQUACY ROOT EARLY_ERRORS x c.
+  apply strictly_nullable_analysis_correct with (ctx:=ctx) (rer:=rer) (dir:=dir) (m:=m) in STRICTLY_NULLABLE; auto.
+  (* we know that we have enouh fuel to do a single iteration *)
+  destruct (repeatMatcherFuel 0 x) eqn:FUEL; try solve[unfold repeatMatcherFuel in FUEL; lia].
+  simpl. repeat rewrite PeanoNat.Nat.add_sub.
+  destruct (List.Update.Nat.Batch.update None (captures x) (List.Range.Nat.Bounds.range (countLeftCapturingParensBefore r ctx) (countLeftCapturingParensBefore r ctx + countLeftCapturingParensWithin r ctx))) as [xupd|] eqn:UPD.
+  (* The update for clearing internal registers cannot fail *)
+  2: { admit. }
+  (* we are valid after the update *)
+  assert (UPDVALID: Valid (input x) rer (match_state (input x) (endIndex x) xupd)).
+  { admit. }
+  unfold strictly_nullable_matcher in STRICTLY_NULLABLE.
+  specialize (STRICTLY_NULLABLE (match_state (input x) (endIndex x) xupd)
+              (fun y : MatchState =>
+       if (endIndex y =? endIndex x)%Z
+       then None
+       else
+        repeatMatcher' m 0 +∞ true y c (countLeftCapturingParensBefore r ctx)
+                              (countLeftCapturingParensWithin r ctx) n) UPDVALID).
+  destruct STRICTLY_NULLABLE as [MISMATCH | [y [VALIDy [SAMEIDX EQUAL]]]].
+  - rewrite MISMATCH. simpl. auto.
+  - rewrite <- EQUAL. simpl in SAMEIDX. rewrite SAMEIDX. rewrite Z.eqb_refl.
+    simpl. auto.
+Admitted.
+(* we have to prove that the update goes well and preserves validity, but I expect this to be feasible *)
 
-(** * Transformation Correctness  *)
+
+
+
+(** * Transformation Correctness -- DEPRECATED *)
+(* I'm keeping this version around to remember how I reasoned about updates and validity *)
 (* Correctness of the transformation that replaces the star of a strictly nullable regex with epsilon *)
 
 (* The repeatMatcher', whether it compiles r* or empty when r is strictly nullable, *)
 (* returns matchers equal matchers *)
 (* this requires that rer is correct with regards to the root regex being compiled *)
-Lemma strictly_nullable_repeatmatcher':
+Lemma strictly_nullable_repeatmatcher_deprecated:
   forall (r:Regex) (root:Regex) (ctx:RegexContext) (rer:RegExp) (dir:direction) (m:Matcher)
     (STRICTLY_NULLABLE: strictly_nullable r = true)
     (COMPILE: compileSubPattern r ctx rer dir = Success m)
@@ -74,14 +165,12 @@ Proof.
   (* disjunction *)
   -
     (* I realize that induction over r is not a good idea. *)
-    (* (r1|r2)* is not r1*|r2* so how do I use my Induction Hypothesis? *)
-    
-    
-      
+    (* (r1|r2)* is not r1*|r2* so how do I use my Induction Hypothesis? *)      
       Admitted.
 
 
-    
+(** * Transformation correctness: Switching a strictly nullable regex starred for a n empty is correct  *)
+
 Theorem strictly_nullable_same_matcher:
   forall (r:Regex) (root:Regex) (ctx:RegexContext) (rer:RegExp) (dir:direction) (mstar:Matcher) (mempty:Matcher)
     (STRICTLY_NULLABLE: strictly_nullable r = true)

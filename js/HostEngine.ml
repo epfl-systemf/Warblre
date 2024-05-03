ebcd2f2e969f2b1edf35d0439e4715c4922e8cb6
Noe De Santo <noe@ef5.ch>
2024-05-03 15:19:44 +0200

    Fix OCaml to Js string conversion
    Conversion which did not support Unicode were being used in some places.

--- js/HostEngine.ml
+++ js/HostEngine.ml
@@ -10,11 +10,11 @@ module HostEngine (P: Engines.EngineParameters) (S: Encoding.StringLike with typ
   let initialize (regex: (character, string) Extracted.Patterns.coq_Regex) (flags: Extracted.RegExpFlags.coq_type): regex =
     let regex_string = Printer.regex_to_string ~delimited:false ~strict:true regex in
     let flags_string = Printer.flags_to_string flags in
-    new%js Js.regExp_withFlags (Js.bytestring regex_string) (Js.string flags_string)
+    new%js Js.regExp_withFlags (Js.string regex_string) (Js.string flags_string)
 
   let setLastIndex (r: regex) (at: int): unit = r##.lastIndex := at
 
   let exec (r: regex) (str: string): (character, string) Extracted.ExecArrayExotic.coq_type option =
-    let js_result = r##exec (Js.bytestring (S.to_string str)) in
+    let js_result = r##exec (Js.string (S.to_string str)) in
     Conversion.MatchResult.ocaml_of_js js_result
 end

51317afbe3752e7e67c2ebf1a746237f6d7bd854
Noe De Santo <noe@ef5.ch>
2024-05-07 15:49:11 +0200

    Add rule for browser fuzzer

---
+++ fuzzer/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+      name: 'warblre-browser-fuzzer',
+      mode: 'production',
+      target: 'web',
+      output: {
+        filename: './warblre-browser-fuzzer.js'
+      },
+    }
+  ];

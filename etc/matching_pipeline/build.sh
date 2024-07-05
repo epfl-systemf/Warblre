3093c44bf6d20ce016ac3352ba48e5f6d3868192
Noe De Santo <noe@ef5.ch>
2024-07-05 14:41:42 +0200

    Improve pipeline picture

--- etc/matching_pipeline/build.sh
+++ etc/matching_pipeline/build.sh
@@ -3,4 +3,4 @@
 BUILD_DIR="_build"
 mkdir -p ${BUILD_DIR}
 lualatex --output-directory=${BUILD_DIR} --output-format=dvi picture.tex
-dvisvgm ${BUILD_DIR}/picture.dvi
+dvisvgm --no-fonts -b 10 ${BUILD_DIR}/picture.dvi

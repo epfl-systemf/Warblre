ccd8ea1994f9f47521a2ca652e571fdaf8ad61ee
Aurèle <aurele.barriere@epfl.ch>
2024-08-15 16:31:26 +0200

    Update Notation.v

--- mechanization/spec/Notation.v
+++ mechanization/spec/Notation.v
@@ -4,7 +4,7 @@ From Warblre Require Import Typeclasses Result Numeric Characters Errors Paramet
     WILDCARD Sections
       ["22.2.2.1"]
 <<*)
-(* + The section 22.2.2.1 does not match its the ECMASCript specification as is because of its unique format. + *)
+(* + The section 22.2.2.1 does not match the ECMASCript specification as is because of its unique format. + *)
 (** >>
     22.2.2.1 Notation
 

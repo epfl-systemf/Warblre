ce63b3ac066627c8bf8b6d5be95cdd45c87d4f3c
Noe De Santo <noe@ef5.ch>
2024-09-26 14:51:56 -0400

    WIP

--- flake.nix
+++ flake.nix
@@ -2,44 +2,48 @@
   description = "A mechanization of the specification of ECMAScript regexes.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    melange = {
-      url = "github:melange-re/melange/v3-414";
+    # melange = {
+    #   url = "github:melange-re/melange/4.0.0-414";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.flake-utils.follows = "flake-utils";
+    # };
+
+    ocaml-overlay = {
+      url = "github:nix-ocaml/nix-overlays";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, melange }@input: 
+  outputs = { self, nixpkgs, flake-utils, ocaml-overlay }@input: 
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ melange.overlays.default ]; };
-        pkgs-unstable = import nixpkgs-unstable { inherit system; };
+        pkgs = import nixpkgs { inherit system; overlays = [ ocaml-overlay.overlays.default ]; };
       in {
         devShells = {
-            default = pkgs.mkShell {
-              buildInputs = with pkgs; [
-                coq
+          default = pkgs.mkShell {
+            buildInputs = (with pkgs; [
+              # coq
 
-                ocaml
-                pkgs-unstable.dune_3
-                ocamlPackages.ocamlformat
-                ocamlPackages.ocaml-lsp
-                ocamlPackages.findlib
-                ocamlPackages.integers
-                ocamlPackages.uucp
-                ocamlPackages.ppx_expect
-                ocamlPackages.melange
-                ocamlPackages.zarith
+              # coqPackages.serapi
+              # python311Packages.alectryon
 
-                coqPackages.serapi
-                python311Packages.alectryon
-
-                nodejs_21
-                nodePackages.webpack-cli
-              ];
+              nodejs_22
+              nodePackages.webpack-cli
+            ]) ++ (with pkgs.ocaml-ng.ocamlPackages_4_14; [
+              ocaml
+              dune_3
+              # ocamlformat
+              ocaml-lsp
+              findlib
+              integers
+              # uucp
+              ppx_expect
+              # melange.packages."${system}".melange
+              melange
+              zarith
+            ]);
           };
         };
       });
